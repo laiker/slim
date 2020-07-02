4c9ec4a458bbd367484a0a8c69ad8834155052b9
Ruslan <laikernest@gmail.com>
2020-07-02 16:56:35 +0000

    Route users with search

--- public/index.php
+++ public/index.php
@@ -2,8 +2,13 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
@@ -11,20 +16,39 @@ $app->get('/', function ($request, $response) {
     return $response;
 });
 
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
+$app->get('/courses/{courseId}/lessons/{id}', function ($request, $response, array $args) {
+    $courseId = $args['courseId'];
+    $id = $args['id'];
+    return $response->write("Course id: {$courseId}")->write("Lesson id: {$id}");
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+
+$app->get('/users', function ($request, $response, $args) use ($users) {
+    $term = $request->getQueryParam('term');
+    if ($term) {
+
+        foreach ($users as $userName) {
+            if (strpos($userName, $term) !== false) {
+                $newusers[] = $userName;
+            }
+        }
+        
+        $users = $newusers;
+    }
+    $params['users'] = $users;
+    $params['term'] = $term;
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 });
 
 $app->post('/users', function ($request, $response) {
     $newResponse = $response->withStatus(302);
     return $newResponse;
-});
-
-$app->get('/courses/{courseId}/lessons/{id}', function ($request, $response, array $args) {
-    $courseId = $args['courseId'];
-    $id = $args['id'];
-    return $response->write("Course id: {$courseId}")
-      ->write("Lesson id: {$id}");
 });
 
 
